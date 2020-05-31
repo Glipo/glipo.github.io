aff27b28a4651662561ae8972761e69794f55dce
James-Livesey <jamesliveg@gmail.com>
2020-05-31 16:17:09 +0100

    Add server script so that 404 pages can be resolved correctly

---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+    Glipo
+
+    Copyright (C) Glipo Technologies. All Rights Reserved.
+
+    https://glipo.cf
+*/
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBxs_F52qiFI85ZbFQ7ysIrvBhKDEvutuw",
+    authDomain: "glipo-net.firebaseapp.com",
+    databaseURL: "https://glipo-net.firebaseio.com",
+    projectId: "glipo-net",
+    storageBucket: "glipo-net.appspot.com",
+    messagingSenderId: "991734469429",
+    appId: "1:991734469429:web:93a3a72653c3921831f27c",
+    measurementId: "G-MGPY4PE9JH"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
