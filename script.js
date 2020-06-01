1643681bf2405dfed6b9b942b847f648916441b6
James-Livesey <jamesliveg@gmail.com>
2020-06-01 10:58:00 +0100

    Add account system

--- script.js
+++ script.js
@@ -20,6 +20,13 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
+var currentUser = {
+    uid: null,
+    username: null
+};
+
+var accountRequiresSettingUp = false;
+
 function showMenu() {
     $(".menu").show();
     $(".locationDropdownIndicator").text("arrow_drop_up");
@@ -65,19 +72,137 @@ function switchToSignUpDialog() {
 }
 
 function switchToSignUpUsernameDialog() {
-    if ($("#signUpEmail").val().trim() != "" && $("#signUpPassword").val() != "") {
+    if ($("#signUpEmail").val().trim() != "" && $("#signUpPassword").val().length >= 6) {
         closeDialogs();
 
         $(".signUpUsernameDialog")[0].showModal();
         $("#signUpUsername").focus();
 
         $("#signUpError").text("");
+    } else if ($("#signUpPassword").val() != "") {
+        $("#signUpError").text(_("Your password must be at least 6 characters long."));
     } else {
         $("#signUpError").text(_("Please enter your email address and password before continuing."));
     }
 }
 
+function signUp() {
+    if ($("#signUpUsername").val().match(/^[a-zA-Z0-9]{1,20}$/) && $("#signUpPassword").val().length >= 6) {
+        $("#signUpUsernameButton").prop("disabled", true);
+
+        firebase.firestore().collection("usernames").doc($("#signUpUsername").val().toLowerCase()).get().then(function(document) {
+            if (!document.exists) {
+                accountRequiresSettingUp = true;
+
+                firebase.auth().createUserWithEmailAndPassword($("#signUpEmail").val().trim(), $("#signUpPassword").val()).catch(function(error) {
+                    accountRequiresSettingUp = false;
+
+                    if (error.code == "auth/email-already-in-use") {
+                        $("#signUpUsernameError").text(_("There already appears to be an account with that email address. Please try again with a different one."));
+                    } else if (error.code == "auth/invalid-email") {
+                        $("#signUpUsernameError").text(_("The email you have entered appears to be invalid. Go back, re-enter your email address and try again."));
+                    } else {
+                        $("#signUpUsernameError").text(_("We ran into a problem when creating your account. Please check your internet connection and try again."));
+                    }
+
+                    $("#signUpUsernameButton").prop("disabled", false);
+                });
+            } else {
+                $("#signUpUsernameError").text(_("Sorry, but that username is taken. Try another one!"));
+
+                $("#signUpUsernameButton").prop("disabled", false);
+            }
+        });
+    } else if ($("#signUpUsername").val() == "") {
+        $("#signUpUsernameError").text(_("Please enter your username before signing up."));
+    } else if ($("#signUpEmail").val().trim() == "" || $("#signUpPassword").val().length < 6) {
+        $("#signUpUsernameError").text(_("Please go back and check that your email and password is correct before signing up."));
+    } else {
+        $("#signUpUsernameError").text(_("Your username must only contain letters and numbers, and cannot exceed 20 characters."));
+    }
+}
+
+function signIn() {
+    if ($("#signInEmail").val().trim() != "" && $("#signInPassword").val() != "") {
+        $("#signInButton").prop("disabled", true);
+
+        firebase.auth().signInWithEmailAndPassword($("#signInEmail").val().trim(), $("#signInPassword").val()).then(function() {
+            $("#signInButton").prop("disabled", false);
+
+            closeDialogs();
+
+            $("#signInEmail").val("");
+            $("#signInPassword").val("");
+        }).catch(function(error) {
+            if (error.code == "auth/invalid-email") {
+                $("#signUpUsernameError").text(_("The email you have entered appears to be invalid. Go back, re-enter your email address and try again."));
+            } else if (error.code == "auth/user-not-found") {
+                $("#signUpUsernameError").text(_("There appears to be no user with that email address. Did you mean to sign up instead?"));
+            } else if (error.code == "auth/wrong-password") {
+                $("#signUpUsernameError").text(_("The password that you have entered is wrong and doesn't match this account's password. Try typing it in again."));
+            } else {
+                $("#signUpUsernameError").text(_("We ran into a problem when signing into your account. Please check your internet connection and try again."));
+            }
+
+            $("#signInButton").prop("disabled", false);
+        });
+    } else {
+        $("#signUpError").text(_("Please enter your email address and password to sign in."));
+    }
+}
+
+function signOut() {
+    firebase.auth().signOut();
+}
+
 $(function() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            currentUser.uid = user.uid;
+
+            if (accountRequiresSettingUp) {
+                firebase.firestore().collection("users").doc(currentUser.uid).set({
+                    username: $("#signUpUsername").val()
+                }).then(function() {
+                    firebase.firestore().collection("usernames").doc($("#signUpUsername").val().toLowerCase()).set({
+                        uid: currentUser.uid
+                    }).then(function() {
+                        $("#signUpUsernameButton").prop("disabled", false);
+
+                        closeDialogs();
+
+                        $("#signUpEmail").val("");
+                        $("#signUpPassword").val("");
+                        $("#signUpUsername").val("");
+                    });
+                });
+
+                currentUser.username = $("#signUpUsername").val();
+
+                $(".currentUsername").text(currentUser.username);
+
+                accountRequiresSettingUp = false;
+            } else {
+                firebase.firestore().collection("users").doc(currentUser.uid).get().then(function(document) {
+                    currentUser.username = document.data().username;
+
+                    $(".currentUsername").text(currentUser.username);
+                });
+            }
+
+            $(".signedOut").hide();
+            $(".signedIn").show();
+        } else {
+            currentUser.uid = null;
+            currentUser.username = null;
+
+            $(".signedIn").hide();
+            $(".signedOut").show();
+
+            $(".currentUsername").text("");
+        }
+    });
+
     $("html").mouseup(function(event) {
         if (!$(".location, .menuButton").is(event.target) && $(".location, .menuButton").has(event.target).length == 0) {
             hideMenu();
@@ -89,6 +214,24 @@ $(function() {
             $(this).css("outline", "none").on("blur", function() {
                 $(this).off("blur").css("outline", "");
             });
+        }
+    });
+
+    $("#signInPassword").keypress(function(event) {
+        if (event.keyCode == 13) {
+            signIn();
+        }
+    });
+
+    $("#signUpPassword").keypress(function(event) {
+        if (event.keyCode == 13) {
+            switchToSignUpUsernameDialog();
+        }
+    });
+
+    $("#signUpUsername").keypress(function(event) {
+        if (event.keyCode == 13) {
+            signUp();
         }
     });
 });
